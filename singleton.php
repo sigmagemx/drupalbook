d81964cfe7a0d684f6c4a0ce3a0df3423cf04836
Evgenija <sigmagemx@gmail.com>
2017-03-11 10:54:15 +0300

    Finish singleton pattern

---
+++ singleton.php
@@ -0,0 +1,24 @@
+<?php
+
+class DB
+{
+	private static $singleton;
+
+	private function __construct()
+	{
+
+	}
+
+	public static function getInstance()
+	{
+		if (!self::$singleton) {
+			self::$singleton = new DB();
+		}
+
+		return self::$singleton;
+	}
+}
+
+$db = DB::getInstance();
+
+?>
